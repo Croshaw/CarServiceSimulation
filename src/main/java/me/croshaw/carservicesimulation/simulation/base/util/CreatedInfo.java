df7eec18b755f8b469efd25e8f989a4de3a57abe
Croshaw <crosafixx200215@gmail.com>
2023-12-23 20:35:13 +0500

    feat: added CreatedInfo class

---
+++ src/main/java/me/croshaw/carservicesimulation/simulation/base/util/CreatedInfo.java
@@ -0,0 +1,31 @@
+package me.croshaw.carservicesimulation.simulation.base.util;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+public class CreatedInfo {
+    private final LocalDateTime dateTimeOfCreation;
+
+    public CreatedInfo() {
+        this(LocalDateTime.of(LocalDate.now(), LocalTime.of(0,0,0)));
+    }
+    public CreatedInfo(LocalDateTime dateTimeOfCreation) {
+        this.dateTimeOfCreation = dateTimeOfCreation;
+    }
+
+    public LocalDateTime getDateTimeOfCreation() {
+        return dateTimeOfCreation;
+    }
+    public LocalDate getDateOfCreation() {
+        return dateTimeOfCreation.toLocalDate();
+    }
+    public LocalTime getTimeOfCreation() {
+        return dateTimeOfCreation.toLocalTime();
+    }
+    @Override
+    public String toString() {
+        return "Дата создания: %s".formatted(dateTimeOfCreation.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
+    }
+}
